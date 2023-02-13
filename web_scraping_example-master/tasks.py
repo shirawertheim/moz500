15bd185981d73ff4ab2cbc730cd8b6f0b9ddd809
shira wertheim <shira.wert@gmail.com>
2023-02-13 15:02:29 +0200

    working version with one worker

--- web_scraping_example-master/tasks.py
+++ web_scraping_example-master/tasks.py
@@ -1,93 +1,31 @@
-from celery import Celery
-import requests
-from bs4 import BeautifulSoup
 import hashlib
 import json
-from collections import namedtuple
 
+import requests
+from bs4 import BeautifulSoup
+from celery import Celery
 
-
-from celery.schedules import crontab # scheduler
-from celery import group
-# from tasks import urlopen
-
-NUMBER_OF_DOMAINS=20
-# app = Celery('tasks', broker_url='redis://127.0.0.1:6379/1')
+NUMBER_OF_DOMAINS = 20
 app = Celery('tasks', broker='pyamqp://guest@localhost//', backend='rpc://')
+site_list = []
 
-# dictionary = []
-
-# app = Celery('tasks')
-# app.conf.update(
-#     worker_concurrency=5,
-#     worker_max_tasks_per_child=100,
-#     worker_pool='gevent',
-#     task_acks_late=True,
-#     task_reject_on_worker_lost=True,
-# )
+@app.task
+def appened_to_list(result_list, result):
+    return result_list
 
 
-# app = Celery('tasks')
-# app.conf.timezone = 'UTC'
-# app.conf.beat_schedule = {
-#     # executes every 1 minute
-#     'scraping-task-one-min': {
-#         'task': 'tasks.create_domain_obj',
-#         'schedule': crontab(),
-#     },
-#     # # executes every 15 minutes
-#     # 'scraping-task-fifteen-min': {
-#     #     'task': 'tasks.hackernews_rss',
-#     #     'schedule': crontab(minute='*/15')
-#     # },
-#     # # executes daily at midnight
-#     # 'scraping-task-midnight-daily': {
-#     #     'task': 'tasks.hackernews_rss',
-#     #     'schedule': crontab(minute=0, hour=0)
-#     # }
-# }
-
-# app.conf.update(
-#     task_acks_late=True,
-#     worker_concurrency=5
-# )
-
-def initialize():
-    print("initialize")
-    # app.start(argv=['celery', 'worker', '-c', '20', '-l', 'INFO'])
-    domains = crawl()
-    # worker = app.Worker(concurrency=5)
-    # worker.start()
-    initialize_dictionary_domains(domains)
-    # write_to_file(dictionary)
-    print("hello")
-
-
-
-def initialize_dictionary_domains(domains):
-    print("initialize_dictionary_domains")
-    result_dict = []
+def initialize_list_domains(domains):
+    print("initialize_list_domains")
+    result_list = []
+    # result_list.append((group(create_domain_obj.delay(obj).get() for obj in domains)))
     for obj in domains:
-        # result = create_domain_obj.apply_async(args=[obj])
         result = create_domain_obj(obj)
-        result_dict.append(result)
+        result_list.append(result)
 
-    return result_dict
-
-# @app.task
-# def initialize_dictionary_domains(domains):
-#     print("initialize_dictionary_domains")
-#     print()
-#     # dictionary = []
-#     result = group(create_domain_obj(url)
-#                    for url in domains).apply_async()
-#     for incoming_result in result.iter_native():
-#         print(incoming_result)
-
+    return result_list
 
 def crawl():
     print("crawl")
-
     url = "https://moz.com/top500/"
 
     headers = {
@@ -109,7 +47,6 @@ def get_html(url,headers):
 
     return ''
 
-
 def extract_domains(soup):
     domains = []
     for i, tr in enumerate(soup.find_all("tr")):
@@ -121,13 +58,9 @@ def extract_domains(soup):
 
     return domains
 
-
-
 @app.task
 def create_domain_obj(url):
-    print("create_domain_obj")
-    data = ["", [], ""]
-
+    data = {"title": "", "links": [], "favicon_hash": ""}
     try:
         response = requests.get(url)
 
@@ -136,7 +69,7 @@ def create_domain_obj(url):
 
             # Extract the title
             title = soup.title.string if soup.title else "Title not found"
-            data[0] = title
+            data["title"] = title
 
             # Extract the links
             links = []
@@ -145,32 +78,31 @@ def create_domain_obj(url):
                 if href:
                     links.append(href)
 
-            data[1] = links
+            data["links"] = links
 
             # Extract the hash of the website's favicon
             favicon_url = url + "/favicon.ico"
             favicon = requests.get(favicon_url)
             favicon_hash = hashlib.md5(
                 favicon.content).hexdigest() if favicon.status_code == 200 else "Favicon not found"
-            data[2] = favicon_hash
+            data["favicon_hash"] = favicon_hash
 
     except Exception as e:
-        data[0] = "Title not found"
-        data[1] = []
-        data[2] = "Favicon not found"
+        data["title"] = "Title not found"
+        data["links"] = []
+        data["favicon_hash"] = "Favicon not found"
         print(f"An exception occurred: {e}")
-
-    print(data)
+    # print(data)
     return data
 
-
-def write_to_file(dictionary):
+def write_to_file(site_list):
     with open("websites_data.json", "w") as file:
-        json.dump(dictionary, file)
-
+        json.dump(site_list, file)
 
 if __name__ == '__main__':
     print('Starting scraping')
-    initialize()
+    domains = crawl()
+    site_list = initialize_list_domains(domains)
+    write_to_file(site_list)
     print('Finished scraping')
 
